c5bba55bc6c6fe30b531a523a5f71a2a379822d1
idoyudha <idowidya.yudhatama@gmail.com>
2024-11-10 19:37:29 +0700

    feat: kafka config data

---
+++ kafka/kafka_config_data/src/main/java/multiwarehouse/ecommerce/KafkaConfigData.java
@@ -0,0 +1,16 @@
+package multiwarehouse.ecommerce;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "kafka_config")
+public class KafkaConfigData {
+    private String bootstrapServers;
+    private String schemaRegistryUrlKey;
+    private String schemaRegistryUrl;
+    private Integer numOfPartitions;
+    private Short replicationFactor;
+}
