be042d2ad36e61e09468b2fbc4ef06f9453e6935
idoyudha <idowidya.yudhatama@gmail.com>
2024-11-10 15:53:32 +0700

    feat: common application

--- common/common_application/src/main/java/multiwarehouse/ecommerce/handler/ErrorDTO.java
+++ common/common_application/src/main/java/multiwarehouse/ecommerce/handler/ErrorDTO.java
@@ -1,4 +1,13 @@
 package multiwarehouse.ecommerce.handler;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
 public class ErrorDTO {
+    private final String code;
+    private final String message;
 }
