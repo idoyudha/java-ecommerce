e538ab175d7729b9f4c903abd6d2cf2a7fca2b43
Ido Widya Yudhatama <idowidya.yudhatama@gmail.com>
2024-11-12 11:51:44 +0700

    feat: paymentResponseAvroModel.avsc

---
+++ order_service/order_messaging/src/main/java/multiwarehouse/ecommerce/listener/kafka/PaymentResponseKafkaListener.java
@@ -0,0 +1,9 @@
+package multiwarehouse.ecommerce.listener.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class PaymentResponseKafkaListener {
+}
