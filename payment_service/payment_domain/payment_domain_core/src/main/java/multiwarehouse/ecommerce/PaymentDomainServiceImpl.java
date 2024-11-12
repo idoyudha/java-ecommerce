39d9b0f64807189cd11791e36093d025285643fa
Ido Widya Yudhatama <idowidya.yudhatama@gmail.com>
2024-11-12 17:15:01 +0700

    feat: add log in payment domain and delete createPayment function

--- payment_service/payment_domain/payment_domain_core/src/main/java/multiwarehouse/ecommerce/PaymentDomainServiceImpl.java
+++ payment_service/payment_domain/payment_domain_core/src/main/java/multiwarehouse/ecommerce/PaymentDomainServiceImpl.java
@@ -1,13 +1,16 @@
 package multiwarehouse.ecommerce;
 
+import lombok.extern.slf4j.Slf4j;
 import multiwarehouse.ecommerce.entity.Payment;
 import multiwarehouse.ecommerce.event.PaymentCreatedEvent;
 import multiwarehouse.ecommerce.event.publisher.DomainEventPublisher;
 
+@Slf4j
 public class PaymentDomainServiceImpl implements PaymentDomainService {
     @Override
     public PaymentCreatedEvent createPayment(Payment payment, DomainEventPublisher<PaymentCreatedEvent> PaymentCreatedEventDomainEventPublisher) {
-        payment.createPayment();
+        payment.initializePayment();
+//        log.info("Payment with id: {} already initialized", payment.getId().getValue());
         return new PaymentCreatedEvent(payment, PaymentCreatedEventDomainEventPublisher);
     }
 }
