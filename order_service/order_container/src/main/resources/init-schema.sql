9ff0702fca9426607354b233dceb57fb426e5d85
Ido Widya Yudhatama <idowidya.yudhatama@gmail.com>
2024-11-12 11:06:35 +0700

    feat: order container and fix payment schema

---
+++ order_service/order_container/src/main/resources/init-schema.sql
@@ -0,0 +1,64 @@
+DROP SCHEMA IF EXISTS "order" CASCADE;
+
+CREATE SCHEMA "order";
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+DROP TYPE IF EXISTS order_status;
+CREATE TYPE order_status AS ENUM ('PENDING', 'PAID', 'FAILED');
+
+DROP TABLE IF EXISTS "order"."orders";
+
+CREATE TABLE "order"."orders" (
+  "id" UUID NOT NULL,
+  "user_id" UUID NOT NULL,
+  "order_status" order_status NOT NULL,
+  "payment_id" UUID UNIQUE,
+  "total_price" NUMERIC(10,2) NOT NULL,
+  "note" CHARACTER VARYING COLLATE pg_catalog."default" NOT NULL,
+  "created_at" TIMESTAMP WITH TIME ZONE NOT NULL,
+  CONSTRAINT orders_pkey PRIMARY KEY ("id")
+);
+  
+CREATE INDEX idx_user_id ON "orders"."orders" ("user_id");
+CREATE INDEX idx_payment_reference_id ON "orders"."orders" ("payment_id");
+  
+DROP TABLE IF EXISTS "order".order_items CASCADE;
+
+CREATE TABLE "order".order_items (
+  "id" UUID NOT NULL,
+  "order_id" UUID UNIQUE NOT NULL,
+  "product_id" UUID NOT NULL,
+  "quantity" UUID NOT NULL,
+  "price" NUMERIC(10,2) NOT NULL,
+  "subtotal" NUMERIC(10,2) NOT NULL,
+  CONSTRAINT order_items_pkey PRIMARY KEY (id, order_id)
+);
+
+ALTER TABLE "order".order_items
+ADD CONSTRAINT "FK_ORDER_ID" FOREIGN KEY (order_id)
+REFERENCES "order".orders (id) MATCH SIMPLE
+ON UPDATE NO ACTION
+ON DELETE CASCADE
+NOT VALID;
+
+DROP TABLE IF EXISTS "order".order_address CASCADE;
+
+CREATE TABLE "order".order_address (
+  "id" UUID NOT NULL,
+  "order_id" UUID UNIQUE NOT NULL,
+  "address" CHARACTER VARYING COLLATE pg_catalog."default" NOT NULL,
+  "city" CHARACTER VARYING COLLATE pg_catalog."default" NOT NULL,
+  "state" CHARACTER VARYING COLLATE pg_catalog."default" NOT NULL,
+  "country" CHARACTER VARYING COLLATE pg_catalog."default" NOT NULL,
+  "zipcode" CHARACTER VARYING COLLATE pg_catalog."default" NOT NULL,
+  CONSTRAINT order_address_pkey PRIMARY KEY (id, order_id)
+)
+
+ALTER TABLE "order".order_address
+ADD CONSTRAINT "FK_ORDER_ID" FOREIGN KEY (order_id)
+REFERENCES "order".orders (id) MATCH SIMPLE
+ON UPDATE NO ACTION
+ON DELETE CASCADE
+NOT VALID;
+
--- payment_service/payment_container/src/main/resources/init-schema.sql
+++ payment_service/payment_container/src/main/resources/init-schema.sql
@@ -10,17 +10,17 @@ CREATE TYPE payment_status AS ENUM ('ONPROCESS', 'SUCCESS', 'FAILED');
 DROP TYPE IF EXISTS payment_method;
 CREATE TYPE payment_method AS ENUM ('DEBIT', 'CREDIT', 'CASH', 'QRIS');
 
-DROP TABLE IF EXISTS "payment"."payment";
+DROP TABLE IF EXISTS "payment"."payments";
 
-CREATE TABLE "payment"."payment" (
+CREATE TABLE "payment"."payments" (
     "id" UUID NOT NULL,
-    "reference_id" UUID NOT NULL,
+    "reference_id" UUID UNIQUE NOT NULL,
     "payment_method" payment_method NOT NULL,
     "payment_status" payment_status NOT NULL,
-    "failed_message" VARCHAR(255),
-    "amount" DECIMAL NOT NULL,
+    "failed_message" CHARACTER VARYING COLLATE pg_catalog."default" NOT NULL,
+    "amount" NUMERIC(10,2) NOT NULL,
     "created_at" TIMESTAMP WITH TIME ZONE NOT NULL,
-    PRIMARY KEY ("id")
+    CONSTRAINT payments_pkey PRIMARY KEY ("id")
 );
 
-CREATE INDEX idx_payment_reference_id ON "payment"."payment" ("reference_id");
+CREATE INDEX idx_payment_reference_id ON "payment"."payments" ("reference_id");
