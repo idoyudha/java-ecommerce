9f828d5a8f8cbab6729ab71a69eb7f7299a3eea4
idoyudha <idowidya.yudhatama@gmail.com>
2024-11-13 17:00:16 +0700

    fix: infra docker

--- ecommerce.sh
+++ ecommerce.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-COMMON_FILE="infra/docker-compose/common.yml"
-ZOOKEEPER_FILE="infra/docker-compose/zookeeper.yml"
-KAFKA_CLUSTER_FILE="infra/docker-compose/kafka_cluster.yml"
-INIT_KAFKA_FILE="infra/docker-compose/init_kafka.yml"
-POSTGRES_FILE="infra/docker-compose/postgres.yml"
+COMMON_FILE="infra/ecommerce/common.yml"
+ZOOKEEPER_FILE="infra/ecommerce/zookeeper.yml"
+KAFKA_CLUSTER_FILE="infra/ecommerce/kafka_cluster.yml"
+INIT_KAFKA_FILE="infra/ecommerce/init_kafka.yml"
+POSTGRES_FILE="infra/ecommerce/postgres.yml"
 
-# Function to check if Docker is running
 check_docker() {
     if ! docker info > /dev/null 2>&1; then
         echo "Docker is not running. Please start Docker and try again."
@@ -19,7 +18,7 @@ start_services() {
     docker compose -f "$COMMON_FILE" -f "$ZOOKEEPER_FILE" up -d
     docker compose -f "$COMMON_FILE" -f "$KAFKA_CLUSTER_FILE" up -d
     docker compose -f "$COMMON_FILE" -f "$INIT_KAFKA_FILE" up -d
-    docker compose -f "$POSTGRES_FILE" -f "$INIT_KAFKA_FILE" up -d
+    docker compose -f "$COMMON_FILE" -f "$POSTGRES_FILE" up -d
 }
 
 show_status() {
@@ -27,7 +26,7 @@ show_status() {
     docker compose -f "$COMMON_FILE" -f "$ZOOKEEPER_FILE" ps
     docker compose -f "$COMMON_FILE" -f "$KAFKA_CLUSTER_FILE" ps
     docker compose -f "$COMMON_FILE" -f "$INIT_KAFKA_FILE" ps
-    docker compose -f "$POSTGRES_FILE" -f "$INIT_KAFKA_FILE" ps
+    docker compose -f "$COMMON_FILE" -f "$POSTGRES_FILE" ps
 }
 
 stop_services() {
@@ -35,13 +34,11 @@ stop_services() {
     docker compose -f "$COMMON_FILE" -f "$ZOOKEEPER_FILE" down
     docker compose -f "$COMMON_FILE" -f "$KAFKA_CLUSTER_FILE" down
     docker compose -f "$COMMON_FILE" -f "$INIT_KAFKA_FILE" down
-    docker compose -f "$POSTGRES_FILE" -f "$INIT_KAFKA_FILE" down
+    docker compose -f "$COMMON_FILE" -f "$POSTGRES_FILE" down
 }
 
-# Check if Docker is running
 check_docker
 
-# Parse command-line arguments
 case "$1" in
     start)
         start_services
