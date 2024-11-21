f3f02d8879aa634115f9034fbfd6fb1012f16929
idoyudha <idowidya.yudhatama@gmail.com>
2024-11-21 23:03:42 +0700

    feat: payment test config

--- payment_service/payment_domain/payment_application_service/src/test/java/multiwarehouse/ecommerce/PaymentTestConfiguration.java
+++ payment_service/payment_domain/payment_application_service/src/test/java/multiwarehouse/ecommerce/PaymentTestConfiguration.java
@@ -1,6 +1,7 @@
 package multiwarehouse.ecommerce;
 
 import multiwarehouse.ecommerce.ports.output.message.publisher.payment.PaymentCreatedRequestMessagePublisher;
+import multiwarehouse.ecommerce.ports.output.repository.PaymentRepository;
 import org.mockito.Mockito;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -10,5 +11,15 @@ public class PaymentTestConfiguration {
     @Bean
     public PaymentCreatedRequestMessagePublisher paymentCreatedRequestMessagePublisher() {
         return Mockito.mock(PaymentCreatedRequestMessagePublisher.class);
+    }
+
+    @Bean
+    public PaymentRepository paymentRepository() {
+        return Mockito.mock(PaymentRepository.class);
+    }
+
+    @Bean
+    public PaymentDomainService paymentDomainService() {
+        return Mockito.mock(PaymentDomainService.class);
     }
 }
